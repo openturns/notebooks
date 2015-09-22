9b6cbabeff7e0fd2e6d1d4591475090ccfbfd1e5
Julien Schueller <schueller@phimeca.com>
2015-09-22 12:40:23 +0200

    Initial import

---
+++ README.rst
@@ -0,0 +1,4 @@
+.. image:: https://travis-ci.org/openturns/notebooks.svg?branch=master
+    :target: https://travis-ci.org/openturns/notebooks
+
+- http://nbviewer.ipython.org/github/openturns/notebooks/blob/master/axial_stressed_beam.ipynb
---
+++ t_notebook.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+import os
+import glob
+from IPython import nbformat
+from IPython.nbconvert import PythonExporter
+import traceback
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+ipynb_path = current_path
+ipynbs = glob.glob(ipynb_path + '/*.ipynb')
+
+for ipynb in ipynbs:
+    with open(ipynb) as fh:
+        nb = nbformat.reads(fh.read(), 4)
+
+    exporter = PythonExporter()
+
+    # source is a tuple of python source code
+    # meta contains metadata
+    source, meta = exporter.from_notebook_node(nb)
+
+    b_name = os.path.basename(ipynb)
+    print('--', b_name)
+    try:
+        exec(source.encode())
+        print('--', b_name, 'OK')
+    except:
+        print('--', b_name, '***Failed')
+        traceback.print_exc()
